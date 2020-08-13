c8999f769d79fad412ea7fee1bf42e918a507654
Iuliia <54540652+YuLogun@users.noreply.github.com>
2020-08-13 10:59:15 +0400

    revised hooks examples from the lecture

--- src/components/counter/Counter.js
+++ src/components/counter/Counter.js
@@ -1,43 +1,31 @@
-import React, { Component } from 'react';
-import './Counter.css';
+import React, { useState } from "react";
+import "./Counter.css";
 
-class Counter extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      counter: 0,
-    };
-  }
-
-  countNumbHandler(type, value) {
+const Counter = () => {
+  const [counter, setCounter] = useState(0);
+  const countNumbHandler = (type, value) => {
     switch (type) {
-      case 'rise':
-        this.setState({ counter: this.state.counter + value });
+      case "rise":
+        setCounter(counter + value);
         break;
-      case 'sub':
-        this.setState({ counter: this.state.counter - value });
+      case "sub":
+        setCounter(counter - value);
         break;
       default:
         break;
     }
-  }
+  };
 
-  render() {
-    return (
-      <div className="counter">
-       <h1>Counter component</h1>
-       <p>Counter value: {this.state.counter}</p>
-        <div className="counter__buttons--row-around">
-          <button onClick={this.countNumbHandler.bind(this, 'sub', 1)}>
-            отнять 1
-          </button>
-          <button onClick={this.countNumbHandler.bind(this, 'rise', 1)}>
-            прибавить 1
-          </button>
-        </div>
+  return (
+    <div className="counter">
+      <h1>Counter component</h1>
+      <p>Counter value: {counter}</p>
+      <div className="counter__buttons--row-around">
+        <button onClick={() => countNumbHandler("sub", 1)}>отнять 1</button>
+        <button onClick={() => countNumbHandler("rise", 1)}>прибавить 1</button>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 export default Counter;
--- src/components/todo/TodoList.js
+++ src/components/todo/TodoList.js
@@ -1,99 +1,77 @@
-import React, { Component } from 'react';
-import TodoItem from './TodoItem';
-import './TodoList.css'
+import React, { useState, useEffect } from "react";
+import TodoItem from "./TodoItem";
+import "./TodoList.css";
 
+const TodoList = () => {
+  const [date, setDate] = useState(new Date());
+  const [todoList, setTodoList] = useState([
+    {
+      name: "Придти на лекцию",
+      description: "Придти на лекцию летней школы по React хукам",
+      isDone: true,
+    },
+    {
+      name: "Послушать лектора",
+      description: "Прослушать занятие и впитать знание",
+      isDone: true,
+    },
+    {
+      name: "Поделать практику",
+      description: "Поделать практику по созданию хуков",
+      isDone: false,
+    },
+    {
+      name: "Написать пользовательский хук",
+      description: "Создать свой собственный хук",
+      isDone: false,
+    },
+    {
+      name: "Выполнить ДЗ",
+      description: "Выполнить домашнее задание по хукам",
+      isDone: false,
+    },
+  ]);
 
-class TodoList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      date: new Date(),
-      todoList: [
-        {
-          name: 'Придти на лекцию',
-          description: 'Придти на лекцию летней школы по React хукам',
-          isDone: true,
-        },
-        {
-          name: 'Послушать лектора',
-          description: 'Прослушать занятие и впитать знание',
-          isDone: true,
-        },
-        {
-          name: 'Поделать практику',
-          description: 'Поделать практику по созданию хуков',
-          isDone: false,
-        },
-        {
-          name: 'Написать пользовательский хук',
-          description: 'Создать свой собственный хук',
-          isDone: false,
-        },
-        {
-          name: 'Выполнить ДЗ',
-          description: 'Выполнить домашнее задание по хукам',
-          isDone: false,
-        },
-      ],
-    };
-  }
+  const tick = () => {
+    setDate(new Date());
+  };
 
-  tick() {
-    this.setState((prevState) => (
-      {
-        ...prevState,
-        date: new Date(),
-      }
-    ));
-  }
-
-  checkTodoHandler(key) {
-    const todos = this.state.todoList.concat();
+  const checkTodoHandler = (key) => {
+    const todos = todoList.concat();
     todos[key].isDone = !todos[key].isDone;
-    this.setState((prevState) => (
-      {
-        ...prevState,
-        todoList: todos,
-      }
-    ));
-  }
+    setTodoList(todos);
+  };
 
-  getTodoList(todolist) {
+  const getTodoList = (todolist) => {
     return todolist.map((item, key) => (
       <TodoItem
         todo={item}
         key={`todo-item-${key}`}
-        checkHandler={this.checkTodoHandler.bind(this, key)}
+        checkHandler={() => checkTodoHandler(key)}
       />
     ));
-  }
+  };
 
-  componentDidMount() {
-    this.timer = setInterval(
-      () => {
-        this.tick();
-      }, 1000);
-    console.log('запускаем таймер');
-  }
+  useEffect(() => {
+    const timer = setInterval(() => {
+      tick();
+    }, 1000);
+    console.log("запускаем таймер");
+    return () => {
+      clearInterval(timer);
+      console.log("удаляем таймер");
+    };
+  }, []);
 
-  componentWillUnmount() {
-    clearInterval(this.timer);
-    console.log('удаляем таймер');
-  }
-
-  render() {
-    return (
-      <>
-        <h1>
-            Привет! Сейчас: {this.state.date.toLocaleString()}
-        </h1>
-        <div className="todo-list">
-          <h2>Список дел:</h2>
-          {this.getTodoList(this.state.todoList)}
-        </div>
-        </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>Привет! Сейчас: {date.toLocaleString()}</h1>
+      <div className="todo-list">
+        <h2>Список дел:</h2>
+        {getTodoList(todoList)}
+      </div>
+    </>
+  );
+};
 
 export default TodoList;
